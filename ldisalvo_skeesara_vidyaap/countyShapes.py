969bbf89c463e960ba8a03bbd41c3f13ab5c1f8c
Vidya Akavoor <vidyaap@bu.edu>
2019-02-28 16:31:43 -0500

    added database for county shapes

---
+++ ldisalvo_skeesara_vidyaap/countyShapes.py
@@ -0,0 +1,118 @@
+import urllib.request
+import json
+import dml
+import prov.model
+import datetime
+import uuid
+import csv
+import io
+
+
+class countyShapes(dml.Algorithm):
+    contributor = 'ldisalvo_skeesara_vidyaap'
+    reads = []
+    writes = ['ldisalvo_skeesara_vidyaap.countyShape']
+
+    @staticmethod
+    def execute(trial=False):
+        '''Retrieve some data sets (not using the API here for the sake of simplicity).'''
+        startTime = datetime.datetime.now()
+
+        # Set up the database connection.
+        client = dml.pymongo.MongoClient()
+        repo = client.repo
+        repo.authenticate('ldisalvo_skeesara_vidyaap', 'ldisalvo_skeesara_vidyaap')
+
+        url = 'http://datamechanics.io/data/massachusetts_counties.csv'
+
+        csv_string = urllib.request.urlopen(url).read().decode("utf-8")
+        reader = csv.DictReader(io.StringIO(csv_string))
+        response = json.loads(json.dumps(list(reader)))
+
+        new_list = []
+
+        for county in response:
+            new_json = {}
+            new_json['Name'] = county['County Name']
+            new_json['Shape'] = county['geometry']
+            new_json['Geo_ID'] = county['GEO_ID2']
+            new_list += [new_json]
+
+        repo.dropCollection("countyShape")
+        repo.createCollection("countyShape")
+        repo['ldisalvo_skeesara_vidyaap.countyShape'].insert_many(new_list)
+        repo['ldisalvo_skeesara_vidyaap.countyShape'].metadata({'complete': True})
+        print(repo['ldisalvo_skeesara_vidyaap.countyShape'].metadata())
+
+        repo.logout()
+
+        endTime = datetime.datetime.now()
+
+        return {"start": startTime, "end": endTime}
+
+    @staticmethod
+    def provenance(doc=prov.model.ProvDocument(), startTime=None, endTime=None):
+        '''
+            Create the provenance document describing everything happening
+            in this script. Each run of the script will generate a new
+            document describing that invocation event.
+            '''
+
+        # Set up the database connection.
+        client = dml.pymongo.MongoClient()
+        repo = client.repo
+        repo.authenticate('alice_bob', 'alice_bob')
+        doc.add_namespace('alg', 'http://datamechanics.io/algorithm/')  # The scripts are in <folder>#<filename> format.
+        doc.add_namespace('dat', 'http://datamechanics.io/data/')  # The data sets are in <user>#<collection> format.
+        doc.add_namespace('ont',
+                          'http://datamechanics.io/ontology#')  # 'Extension', 'DataResource', 'DataSet', 'Retrieval', 'Query', or 'Computation'.
+        doc.add_namespace('log', 'http://datamechanics.io/log/')  # The event log.
+        doc.add_namespace('bdp', 'https://data.cityofboston.gov/resource/')
+
+        this_script = doc.agent('alg:alice_bob#example',
+                                {prov.model.PROV_TYPE: prov.model.PROV['SoftwareAgent'], 'ont:Extension': 'py'})
+        resource = doc.entity('bdp:wc8w-nujj',
+                              {'prov:label': '311, Service Requests', prov.model.PROV_TYPE: 'ont:DataResource',
+                               'ont:Extension': 'json'})
+        get_found = doc.activity('log:uuid' + str(uuid.uuid4()), startTime, endTime)
+        get_lost = doc.activity('log:uuid' + str(uuid.uuid4()), startTime, endTime)
+        doc.wasAssociatedWith(get_found, this_script)
+        doc.wasAssociatedWith(get_lost, this_script)
+        doc.usage(get_found, resource, startTime, None,
+                  {prov.model.PROV_TYPE: 'ont:Retrieval',
+                   'ont:Query': '?type=Animal+Found&$select=type,latitude,longitude,OPEN_DT'
+                   }
+                  )
+        doc.usage(get_lost, resource, startTime, None,
+                  {prov.model.PROV_TYPE: 'ont:Retrieval',
+                   'ont:Query': '?type=Animal+Lost&$select=type,latitude,longitude,OPEN_DT'
+                   }
+                  )
+
+        lost = doc.entity('dat:alice_bob#lost',
+                          {prov.model.PROV_LABEL: 'Animals Lost', prov.model.PROV_TYPE: 'ont:DataSet'})
+        doc.wasAttributedTo(lost, this_script)
+        doc.wasGeneratedBy(lost, get_lost, endTime)
+        doc.wasDerivedFrom(lost, resource, get_lost, get_lost, get_lost)
+
+        found = doc.entity('dat:alice_bob#found',
+                           {prov.model.PROV_LABEL: 'Animals Found', prov.model.PROV_TYPE: 'ont:DataSet'})
+        doc.wasAttributedTo(found, this_script)
+        doc.wasGeneratedBy(found, get_found, endTime)
+        doc.wasDerivedFrom(found, resource, get_found, get_found, get_found)
+
+        repo.logout()
+
+        return doc
+
+
+'''
+# This is example code you might use for debugging this module.
+# Please remove all top-level function calls before submitting.
+example.execute()
+doc = example.provenance()
+print(doc.get_provn())
+print(json.dumps(json.loads(doc.serialize()), indent=4))
+'''
+
+## eof
