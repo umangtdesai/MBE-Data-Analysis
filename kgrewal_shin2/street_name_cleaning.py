a2022e8505e7631f3bfa0a7f6d1f9973ee049e9a
Kerin Grewal <kgrewal@bu.edu>
2019-03-02 12:58:46 -0500

    adding street name provenance

---
+++ kgrewal_shin2/street_name_cleaning.py
@@ -0,0 +1,31 @@
+import pandas as pd
+
+file_name = 'street-book.csv'
+
+sheet = 'street_book'
+
+df = pd.read_csv(file_name)
+print(df.head(5))
+
+remove_words = ['Street',',', 'Road','Place','Square',';',
+                          'Boulevard','Highway','Avenue','Alley', 'North', 'South', 'Mt.', 'Court',
+                      'Terrace', 'Path', 'Circle', 'Park', 'Way', 'Drive']
+
+pat = r'\b(?:{})\b'.format('|'.join(remove_words))
+
+df['street_name'] = df['street-name'].str.replace(pat, '')
+
+if '-' in df['zipcodes']:
+    zip = df['zipcodes'].split('-')[0]
+    df['zipcodes'] = zip
+
+df = df.drop(df.columns[0], axis=1)
+df = df.drop(['street-name', 'gender2'], axis=1)
+print(df.head(5))
+
+
+out = df.to_json(orient='records')[1:-1].replace('},{', '} {')
+
+with open('boston_street_names.json', 'w') as f:
+    f.write(out)
+
