eec610b8e7ee1d6e9af9d9743b48af294dc3ec6b
SreejaKeesara <skeesara@bu.edu>
2019-03-02 13:15:17 -0500

    added provenance for basic ideology transformations

--- ldisalvo_skeesara_vidyaap/transformationSenateDistrictIdeology.py
+++ ldisalvo_skeesara_vidyaap/transformationSenateDistrictIdeology.py
@@ -1,9 +1,7 @@
 """
 CS504 : transformationSenateDistrictIdeology
 Team : Vidya Akavoor, Lauren DiSalvo, Sreeja Keesara
-Description :
-
-Notes : 
+Description : transformation of state senate election data to determine basic ideology of each senate district
 
 March 01, 2019
 """
@@ -99,48 +97,23 @@ class transformationSenateDistrictIdeology(dml.Algorithm):
         # Set up the database connection.
         client = dml.pymongo.MongoClient()
         repo = client.repo
-        repo.authenticate('alice_bob', 'alice_bob')
-        doc.add_namespace('alg',
-                          'http://datamechanics.io/algorithm/')  # The scripts are in <folder>#<filename> format.
-        doc.add_namespace('dat',
-                          'http://datamechanics.io/data/')  # The data sets are in <user>#<collection> format.
-        doc.add_namespace('ont',
-                          'http://datamechanics.io/ontology#')  # 'Extension', 'DataResource', 'DataSet', 'Retrieval', 'Query', or 'Computation'.
+        repo.authenticate(TEAM_NAME, TEAM_NAME)
+        doc.add_namespace('alg', 'http://datamechanics.io/algorithm/')  # The scripts are in <folder>#<filename> format.
+        doc.add_namespace('dat', 'http://datamechanics.io/data/')  # The data sets are in <user>#<collection> format.
+        doc.add_namespace('ont', 'http://datamechanics.io/ontology#')  # 'Extension', 'DataResource', 'DataSet', 'Retrieval', 'Query', or 'Computation'.
         doc.add_namespace('log', 'http://datamechanics.io/log/')  # The event log.
-        doc.add_namespace('bdp', 'https://data.cityofboston.gov/resource/')
 
-        this_script = doc.agent('alg:alice_bob#example',
-                                {prov.model.PROV_TYPE: prov.model.PROV['SoftwareAgent'],
-                                 'ont:Extension': 'py'})
-        resource = doc.entity('bdp:wc8w-nujj', {'prov:label': '311, Service Requests',
-                                                prov.model.PROV_TYPE: 'ont:DataResource',
-                                                'ont:Extension': 'json'})
-        get_found = doc.activity('log:uuid' + str(uuid.uuid4()), startTime, endTime)
-        get_lost = doc.activity('log:uuid' + str(uuid.uuid4()), startTime, endTime)
-        doc.wasAssociatedWith(get_found, this_script)
-        doc.wasAssociatedWith(get_lost, this_script)
-        doc.usage(get_found, resource, startTime, None,
-                  {prov.model.PROV_TYPE: 'ont:Retrieval',
-                   'ont:Query': '?type=Animal+Found&$select=type,latitude,longitude,OPEN_DT'
-                   }
-                  )
-        doc.usage(get_lost, resource, startTime, None,
-                  {prov.model.PROV_TYPE: 'ont:Retrieval',
-                   'ont:Query': '?type=Animal+Lost&$select=type,latitude,longitude,OPEN_DT'
-                   }
-                  )
+        this_script = doc.agent('alg:' + TEAM_NAME + '#transformationSenateDistrictIdeology', {prov.model.PROV_TYPE: prov.model.PROV['SoftwareAgent'], 'ont:Extension': 'py'})
+        stateSenateElectionsEntity = doc.entity('dat:' + TEAM_NAME + '#stateSenateElections', {prov.model.PROV_LABEL: 'MA General State Senate Elections 2000-2018', prov.model.PROV_TYPE: 'ont:DataSet'})
 
-        lost = doc.entity('dat:alice_bob#lost', {prov.model.PROV_LABEL: 'Animals Lost',
-                                                 prov.model.PROV_TYPE: 'ont:DataSet'})
-        doc.wasAttributedTo(lost, this_script)
-        doc.wasGeneratedBy(lost, get_lost, endTime)
-        doc.wasDerivedFrom(lost, resource, get_lost, get_lost, get_lost)
+        get_transformationSenateDistrictIdeology = doc.activity('log:uuid' + str(uuid.uuid4()), startTime, endTime)
+        doc.wasAssociatedWith(get_transformationSenateDistrictIdeology, this_script)
+        doc.usage(get_transformationSenateDistrictIdeology, stateSenateElectionsEntity, startTime, None, {prov.model.PROV_TYPE: 'ont:Retrieval', 'ont:Query': 'distinct district name'})
 
-        found = doc.entity('dat:alice_bob#found', {prov.model.PROV_LABEL: 'Animals Found',
-                                                   prov.model.PROV_TYPE: 'ont:DataSet'})
-        doc.wasAttributedTo(found, this_script)
-        doc.wasGeneratedBy(found, get_found, endTime)
-        doc.wasDerivedFrom(found, resource, get_found, get_found, get_found)
+        transformationSenateDistrictIdeologyEntity = doc.entity('dat:' + TEAM_NAME + '#transformationSenateDistrictIdeology', {prov.model.PROV_LABEL: 'Basic MA State Senate District Ideology Score 2000-2018', prov.model.PROV_TYPE: 'ont:DataSet'})
+        doc.wasAttributedTo(transformationSenateDistrictIdeologyEntity, this_script)
+        doc.wasGeneratedBy(transformationSenateDistrictIdeologyEntity, get_transformationSenateDistrictIdeology, endTime)
+        doc.wasDerivedFrom(transformationSenateDistrictIdeologyEntity, stateSenateElectionsEntity, get_transformationSenateDistrictIdeology, get_transformationSenateDistrictIdeology, get_transformationSenateDistrictIdeology)
 
         repo.logout()
 
