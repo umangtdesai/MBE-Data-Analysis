fe30c45aab9c8425a9b7f95814c4be6d8493fcac
Kerin Grewal <kgrewal@bu.edu>
2019-03-03 20:53:25 -0500

    Added landmarks prov

--- kgrewal_shin2/renaming_streets_prov.py
+++ kgrewal_shin2/renaming_streets_prov.py
@@ -11,7 +11,7 @@ import uuid
 class street_names(dml.Algorithm):
     contributor = 'kgrewal_shin2'
     reads = []
-    writes = ['kgrewal_shin2.street_names']
+    writes = ['kgrewal_shin2.street_names', 'kgrewal_shin2.landmarks']
 
     @staticmethod
     def execute(trial=False):
@@ -32,6 +32,16 @@ class street_names(dml.Algorithm):
         repo['kgrewal_shin2.street_names'].insert_many(r)
         repo['kgrewal_shin2.street_names'].metadata({'complete': True})
         print(repo['kgrewal_shin2.street_names'].metadata())
+
+        url = 'http://datamechanics.io/data/boston_landmarks.json'
+        response = urllib.request.urlopen(url).read().decode("utf-8")
+        r = json.loads(response)
+        s = json.dumps(r, sort_keys=True, indent=2)
+        repo.dropCollection("landmarks")
+        repo.createCollection("landmarks")
+        repo['kgrewal_shin2.landmarks'].insert_many(r)
+        repo['kgrewal_shin2.landmarks'].metadata({'complete': True})
+        print(repo['kgrewal_shin2.landmarks'].metadata())
 
         repo.logout()
 
@@ -64,19 +74,32 @@ class street_names(dml.Algorithm):
                               {'prov:label': '311, Service Requests', prov.model.PROV_TYPE: 'ont:DataResource',
                                'ont:Extension': 'json'})
         get_street_name = doc.activity('log:uuid' + str(uuid.uuid4()), startTime, endTime)
+        get_landmarks = doc.activity('log:uuid' + str(uuid.uuid4()), startTime, endTime)
 
         doc.wasAssociatedWith(get_street_name, this_script)
         doc.usage(get_street_name, resource, startTime, None,
                   {prov.model.PROV_TYPE: 'ont:Retrieval',
-                   'ont:Query': '?type=Animal+Found&$select=type,latitude,longitude,OPEN_DT'
+                   'ont:Query': '?type=Street+Name&$select=full_name,gender,zipcodes,rank,street_name'
                    }
                   )
 
-        lost = doc.entity('dat:kgrewal_shin2#street_name',
-                          {prov.model.PROV_LABEL: 'Animals Lost', prov.model.PROV_TYPE: 'ont:DataSet'})
-        doc.wasAttributedTo(lost, this_script)
-        doc.wasGeneratedBy(lost, get_street_name, endTime)
-        doc.wasDerivedFrom(lost, resource, get_street_name, get_street_name, get_street_name)
+        doc.usage(get_landmarks, resource, startTime, None,
+                  {prov.model.PROV_TYPE:'ont:Retrieval',
+                  'ont:Query':'?type=Landmark+Name&$select=Name of Property,Address,Neighborhood'
+                  }
+                  )
+
+        streets = doc.entity('dat:kgrewal_shin2#street_name',
+                          {prov.model.PROV_LABEL: 'Boston Streets', prov.model.PROV_TYPE: 'ont:DataSet'})
+        doc.wasAttributedTo(streets, this_script)
+        doc.wasGeneratedBy(streets, get_street_name, endTime)
+        doc.wasDerivedFrom(streets, resource, get_street_name, get_street_name, get_street_name)
+
+        landmarks = doc.entity('dat:kgrewal_shin2#street_name',
+                          {prov.model.PROV_LABEL: 'Boston Landmarks', prov.model.PROV_TYPE: 'ont:DataSet'})
+        doc.wasAttributedTo(landmarks, this_script)
+        doc.wasGeneratedBy(landmarks, get_landmarks, endTime)
+        doc.wasDerivedFrom(landmarks, resource, get_landmarks, get_landmarks, get_landmarks)
 
         repo.logout()
 
