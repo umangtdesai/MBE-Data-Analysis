bff5338954566f2fc6df1c0e04e1713d8de191ad
Omar Sagga <osagga@bu.edu>
2019-03-04 00:15:31 -0500

    [Proj 1]  Added back base files

--- reset.js
+++ reset.js
@@ -1,17 +1,17 @@
 /* ****************************************************************************
- ** 
- ** reset.js
- **
- ** Script for setting up and initializing a Data Mechanics Repository instance
- ** within MongoDB.
- **
- ** This script will reset the database so that it is ready for a fresh run of
- ** 'setup.js'; ALL data will be lost!
- **
- **   Web:     datamechanics.org
- **   Version: 0.0.1
- **
- */
+** 
+** reset.js
+**
+** Script for setting up and initializing a Data Mechanics Repository instance
+** within MongoDB.
+**
+** This script will reset the database so that it is ready for a fresh run of
+** 'setup.js'; ALL data will be lost!
+**
+**   Web:     datamechanics.org
+**   Version: 0.0.1
+**
+*/
 
 // Load the configuration file.
 var config = JSON.parse(cat("config.json"));
