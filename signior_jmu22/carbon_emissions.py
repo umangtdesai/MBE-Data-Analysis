36ecd0754838a353d9eb79c446f7eb2d45c2dfed
kevinxia787 <kevinxia787@gmail.com>
2019-03-04 22:57:04 -0500

    added provenance for basic write scripts

--- signior_jmu22/carbon_emissions.py
+++ signior_jmu22/carbon_emissions.py
@@ -20,9 +20,9 @@ class carbon_emissions(dml.Algorithm):
     repo = client.repo
     repo.authenticate('signior_jmu22', 'signior_jmu22')
 
-    url = 'http://datamechanics.io/data/signior_jmu22/co2ppm.json'
-    response = urllib.request.urlopen(url).read().decode("utf-8")
-    r = json.loads(response)
+    url = 'http://datamechanics.io/data/signior_jmu22/co2ppm.json' # grab dataset from datamechanics.io
+    response = urllib.request.urlopen(url).read().decode("utf-8") # open with python request library
+    r = json.loads(response) 
     s = json.dumps(r, sort_keys=True, indent=2)
     repo.dropCollection("carbon_emissions")
     repo.createCollection("carbon_emissions")
@@ -40,11 +40,35 @@ class carbon_emissions(dml.Algorithm):
   
   @staticmethod
   def provenance(doc = prov.model.ProvDocument(), startTime = None, endTime = None):
-    return None
+    client = dml.pymongo.MongoClient()
+    repo = client.repo
+    repo.authenticate('signior_jmu22', 'signior_jmu22')
+    doc.add_namespace('alg', 'http://datamechanics.io/algorithm/signior_jmu22') # The scripts are in <folder>#<filename> format
+    doc.add_namespace('dat', 'http://datamechanics.io/data/signior_jmu22' ) # The datasets are in <user>#<collection> format
+    doc.add_namespace('ont', 'http://datamechanics.io/ontology#') # 'Extension', 'DataResource', 'DataSet', 'Retreival', 'Query', or 'Computation'
+    doc.add_namespace('log', 'http://datamechanics.io/log/')
+
+    this_script = doc.agent('alg:signior_jmu22#carbon_emissions', {prov.model.PROV_TYPE:prov.model.PROV['SoftwareAgent'], 'ont:Extension': 'py'})
+    resource = doc.entity('dat:carbon_emissions', {'prov:label': 'Global CO2 emissions data 1980 - 2017', prov.model.PROV_TYPE: 'ont:DataResource', 'ont:Extension': 'csv'})
+    get_carbon_emissions = doc.activity('log:uuid' + str(uuid.uuid4()), startTime, endTime)
+    doc.wasAssociatedWith(get_carbon_emissions, this_script)
+    doc.usage(get_carbon_emissions, resource, startTime, None, {prov.model.PROV_TYPE:'ont:Retreival'})
+    
+    carbon_emissions = doc.entity('dat:signior_jmu22#carbon_emissions', {prov.model.PROV_LABEL: 'Global Carbon Emissions', prov.model.PROV_TYPE: 'ont:DataSet'})
+    doc.wasAttributedTo(carbon_emissions, this_script)
+    doc.wasGeneratedBy(carbon_emissions, get_carbon_emissions, endTime)
+    doc.wasDerivedFrom(carbon_emissions, resource, get_carbon_emissions, get_carbon_emissions, get_carbon_emissions)
+
+    repo.logout()
+
+
+    return doc
 
 
 
-
-
-
+# comment this when submitting, just for testing purposes
+# carbon_emissions.execute()
+# doc = carbon_emissions.provenance()
+# print(doc.get_provn())
+# print(json.dumps(json.loads(doc.serialize()), indent=4))
   
--- signior_jmu22/power_plants.py
+++ signior_jmu22/power_plants.py
@@ -47,12 +47,36 @@ class power_plants(dml.Algorithm):
   
   @staticmethod
   def provenance(doc = prov.model.ProvDocument(), startTime = None, endTime = None):
-    return None
+    client = dml.pymongo.MongoClient()
+    repo = client.repo
+    repo.authenticate('signior_jmu22', 'signior_jmu22')
+    doc.add_namespace('alg', 'http://datamechanics.io/algorithm/signior_jmu22') # The scripts are in <folder>#<filename> format
+    doc.add_namespace('dat', 'http://datamechanics.io/data/signior_jmu22' ) # The datasets are in <user>#<collection> format
+    doc.add_namespace('ont', 'http://datamechanics.io/ontology#') # 'Extension', 'DataResource', 'DataSet', 'Retreival', 'Query', or 'Computation'
+    doc.add_namespace('log', 'http://datamechanics.io/log/')
+
+    this_script = doc.agent('alg:signior_jmu22#power_plants', {prov.model.PROV_TYPE:prov.model.PROV['SoftwareAgent'], 'ont:Extension': 'py'})
+    resource = doc.entity('dat:power_plants', {'prov:label': 'global_power_plants_database', prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extension':'csv'})
+    get_power_plants_info = doc.activity('log:uuid' + str(uuid.uuid4()), startTime, endTime)
+    doc.wasAssociatedWith(get_power_plants_info, this_script)
+    doc.usage(get_power_plants_info, resource, startTime, None, {prov.model.PROV_TYPE:'ont:Retreival'})
+
+    power_plants_info = doc.entity('dat:signior_jmu22#power_plants', {prov.model.PROV_LABEL: 'Sea Surface Temperature Anomalies', prov.model.PROV_TYPE:'ont:DataSet'})
+    doc.wasAttributedTo(power_plants_info, this_script)
+    doc.wasGeneratedBy(power_plants_info, get_power_plants_info,endTime)
+    doc.wasDerivedFrom(power_plants_info, resource, get_power_plants_info, get_power_plants_info, get_power_plants_info)
+
+    repo.logout()
+
+    return doc
 
 
 
 
 
 
-  
-power_plants.execute()
+# comment this out when submitting, just for testing
+# power_plants.execute()
+# doc = power_plants.provenance()
+# print(doc.get_provn())
+# print(json.dumps(json.loads(doc.serialize()), indent=4))
