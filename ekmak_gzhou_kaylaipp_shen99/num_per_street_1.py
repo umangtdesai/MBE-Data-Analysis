a46258a1720c9712ea5e4990db69fe50268fa569
kaylaipp <kaylaipp@bu.edu>
2019-03-05 16:22:34 -0500

    num per street draft

--- ekmak_gzhou_kaylaipp_shen99/num_per_street_1.py
+++ ekmak_gzhou_kaylaipp_shen99/num_per_street_1.py
@@ -9,26 +9,78 @@ import requests
 import xmltodict
 import csv
 
-
-class num_per_street(dml.Algorithm):
+#number of buildings per street in south boston 
+class num_per_street_1(dml.Algorithm):
     contributor = 'ekmak_gzhou_kaylaipp_shen99'
     reads = ['ekmak_gzhou_kaylaipp_shen99.accessing_data', 'ekmak_gzhou_kaylaipp_shen99.address_data']
-    # writes = ['ekmak_gzhou_kaylaipp_shen99.num_per_street']
-    writes = []
+    writes = ['ekmak_gzhou_kaylaipp_shen99.num_per_street_1']
 
     @staticmethod 
     def execute(trial=False):
         '''Retrieve some data sets (not using the API here for the sake of simplicity).'''
         startTime = datetime.datetime.now()
-
         client = dml.pymongo.MongoClient()
         repo = client.repo
         repo.authenticate('ekmak_gzhou_kaylaipp_shen99','ekmak_gzhou_kaylaipp_shen99')
-        
-        
+
+        #list of tuples in format (street name, street num, 1) aka (beacon st  , 362, 1)                  
+        #contains all street names for accessing & address data = union 
+        all_streets = []
+        #retreive boston address data 
+        address_data = repo.ekmak_gzhou_kaylaipp_shen99.address_data.find()
+        for info in address_data: 
+            zipcode = info['ZIP_CODE']
+            st_full = info['FULL_STREET_NAME'].lower().replace(".", "")  #beacon st
+            st_num = info['STREET_NUMBER_SORT']                          #362
+
+            if zipcode == '02127': 
+                #get list of tuples that have same street name 
+                same_street = list(filter(lambda t: t[0] == st_full, all_streets))
+                #get list of any duplicate buildings
+                same_building = list(filter(lambda t: t[1] == st_num, same_street))
+                #don't add any duplicate buildings 
+                if len(same_building) > 0: 
+                    continue
+                all_streets.append((st_full, st_num, 1))
+
+        #retrieve accessing data
+        accessing_data = repo.ekmak_gzhou_kaylaipp_shen99.accessing_data.find()
+        for info in accessing_data: 
+            zipcode = info['ZIPCODE']
+            st_name = info['ST_NAME'].lower().replace(".", "")         #beacon
+            st_num = info['ST_NUM']                                    #362
+            st_suffix = info['ST_NAME_SUF'].lower().replace(".", "")   #st
+            st_full = st_name + ' ' + st_suffix
+            if zipcode == '02127': 
+                #get list of tuples that have same street name 
+                same_street = list(filter(lambda t: t[0] == st_full, all_streets))
+                #get list of any duplicate buildings
+                same_building = list(filter(lambda t: t[1] == st_num, same_street))
+                #don't add any duplicate buildings 
+                if len(same_building) > 0: 
+                    continue
+                all_streets.append((st_full, st_num, 1))
+
+
+        #take away building numbers t[1], only keep street name and count
+        all_streets = project(all_streets, lambda t: (t[0], t[2]))
+
+        #aggregation by key (st name)
+        total = reduce(lambda street,count: (street, sum(count)), all_streets)
+
+        #create new database 
+        repo.dropCollection("num_per_street_1")
+        repo.createCollection("num_per_street_1")
+
+        #add to database in {street: count} form 
+        for t in total: 
+            repo['ekmak_gzhou_kaylaipp_shen99.num_per_street_1'].insert_one({t[0]:t[1]})
+
+        print('inserted number of houses per street data')
         repo.logout()
         endTime = datetime.datetime.now()
         return {"start":startTime, "end":endTime}
+
 
     @staticmethod
     def provenance(doc = prov.model.ProvDocument(), startTime = None, endTime = None):
@@ -79,6 +131,15 @@ class num_per_street(dml.Algorithm):
                   
         return doc
 
+def map(f, R):
+    return [t for (k,v,s) in R for t in f(k,v)]
+    
+def reduce(f, R):
+    keys = {k for (k,v) in R}
+    return [f(k1, [v for (k2,v) in R if k1 == k2]) for k1 in keys]
+def project(R, p):
+    return [p(t) for t in R]
+
 
 '''
 # This is example code you might use for debugging this module.
@@ -88,4 +149,6 @@ doc = example.provenance()
 print(doc.get_provn())
 print(json.dumps(json.loads(doc.serialize()), indent=4))
 '''
-num_per_street.execute()
+
+num_per_street_1.execute()
+## eof
