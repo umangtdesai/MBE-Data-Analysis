c44cce47a3b3c20e7dd929776817686355079d99
Chu Ci <chuci@Chus-MacBook-Pro.local>
2019-03-04 21:41:27 -0500

    almost finish cc part

--- chuci_yfch_yuwan_zhurh/health_uber_output.py
+++ chuci_yfch_yuwan_zhurh/health_uber_output.py
@@ -17,36 +17,34 @@ class health_uber_output(dml.Algorithm):
         startTime = datetime.datetime.now()
         # Get the uber dataset
         client = dml.pymongo.MongoClient()
-        repo1 = client.repo1
-        print('AAAAAAAAAAAAA')
-        repo1.authenticate('chuci_yfch_yuwan_zhurh', 'chuci_yfch_yuwan_zhurh')
+        repo = client.repo
+        repo.authenticate('chuci_yfch_yuwan_zhurh', 'chuci_yfch_yuwan_zhurh')
         # Aggreate cityname by max population
-        print('BBBBBBBBBBBBB')
         pipline = [
             {'$group': {'_id': "$CityName", 'population': {'$max': "$PopulationCount"}}}
         ]
-        agg_result = repo1['chuci_yfch_yuwan_zhurh.health'].aggregate(pipline)
+        agg_result = repo['chuci_yfch_yuwan_zhurh.health'].aggregate(pipline)
         agg_list = list(agg_result)
-        repo1.dropCollection("population")
-        repo1.createCollection("population")
-        repo1['chuci_yfch_yuwan_zhurh.population'].insert_many(agg_list)
+        repo.dropCollection("population")
+        repo.createCollection("population")
+        repo['chuci_yfch_yuwan_zhurh.population'].insert_many(agg_list)
 
         # Aggreate cityname by mean time
 
         pipline2 = [
             {'$group': {'_id': "$CITY", 'mean_time': {'$avg': "$Mean Travel Time (Seconds)"}}}
         ]
-        agg_result_uber = repo1['chuci_yfch_yuwan_zhurh.uber'].aggregate(pipline2)
+        agg_result_uber = repo['chuci_yfch_yuwan_zhurh.uber'].aggregate(pipline2)
         agg_list_uber = list(agg_result_uber)
-        repo1.dropCollection("uber_mean")
-        repo1.createCollection("uber_mean")
-        repo1['chuci_yfch_yuwan_zhurh.uber_mean'].insert_many(agg_list_uber)
+        repo.dropCollection("uber_mean")
+        repo.createCollection("uber_mean")
+        repo['chuci_yfch_yuwan_zhurh.uber_mean'].insert_many(agg_list_uber)
 
         # use basic relation to join population and mean
 
-        l1 = list(repo1['chuci_yfch_yuwan_zhurh.population'].find())
+        l1 = list(repo['chuci_yfch_yuwan_zhurh.population'].find())
 
-        l2 = list(repo1['chuci_yfch_yuwan_zhurh.uber_mean'].find())
+        l2 = list(repo['chuci_yfch_yuwan_zhurh.uber_mean'].find())
 
         l3 = []
         for each1 in l1:
@@ -56,11 +54,11 @@ class health_uber_output(dml.Algorithm):
                                'population': each1['population'],
                                'mean_time': each2['mean_time']
                                })
-        repo1.dropCollection("city_pop_time")
-        repo1.createCollection("city_pop_time")
-        repo1['chuci_yfch_yuwan_zhurh.city_pop_time'].insert_many(l3)
+        repo.dropCollection("city_pop_time")
+        repo.createCollection("city_pop_time")
+        repo['chuci_yfch_yuwan_zhurh.city_pop_time'].insert_many(l3)
 
-        repo1.logout()
+        repo.logout()
 
         endTime = datetime.datetime.now()
 
@@ -73,8 +71,7 @@ class health_uber_output(dml.Algorithm):
             in this script. Each run of the script will generate a new
             document describing that invocation event.
             '''
-
-        # Set up the database connection.
+        # New
         client = dml.pymongo.MongoClient()
         repo = client.repo
         repo.authenticate('chuci_yfch_yuwan_zhurh', 'chuci_yfch_yuwan_zhurh')
@@ -83,39 +80,26 @@ class health_uber_output(dml.Algorithm):
         doc.add_namespace('ont',
                           'http://datamechanics.io/ontology#')  # 'Extension', 'DataResource', 'DataSet', 'Retrieval', 'Query', or 'Computation'.
         doc.add_namespace('log', 'http://datamechanics.io/log/')  # The event log.
-        doc.add_namespace('bdp', 'https://data.cityofboston.gov/resource/')
-
-        this_script = doc.agent('alg:alice_bob#example',
+        agent = doc.agent('alg:chuci_yfch_yuwan_zhurh#health_uber_output',
                                 {prov.model.PROV_TYPE: prov.model.PROV['SoftwareAgent'], 'ont:Extension': 'py'})
-        resource = doc.entity('bdp:wc8w-nujj',
-                              {'prov:label': '311, Service Requests', prov.model.PROV_TYPE: 'ont:DataResource',
-                               'ont:Extension': 'json'})
-        get_found = doc.activity('log:uuid' + str(uuid.uuid4()), startTime, endTime)
-        get_lost = doc.activity('log:uuid' + str(uuid.uuid4()), startTime, endTime)
-        doc.wasAssociatedWith(get_found, this_script)
-        doc.wasAssociatedWith(get_lost, this_script)
-        doc.usage(get_found, resource, startTime, None,
-                  {prov.model.PROV_TYPE: 'ont:Retrieval',
-                   'ont:Query': '?type=Animal+Found&$select=type,latitude,longitude,OPEN_DT'
-                   }
+        entity_uber = doc.entity('dat:chuci_yfch_yuwan_zhurh#uber',
+                           {prov.model.PROV_LABEL: 'uber data', prov.model.PROV_TYPE: 'ont:DataSet'})
+        entity_health = doc.entity('dat:chuci_yfch_yuwan_zhurh#health',
+                           {prov.model.PROV_LABEL: 'health data', prov.model.PROV_TYPE: 'ont:DataSet'})
+        entity_result = doc.entity('dat:chuci_yfch_yuwan_zhurh#result',
+                                   {prov.model.PROV_LABEL: 'result data', prov.model.PROV_TYPE: 'ont:DataSet'})
+        activity = doc.activity('log:uuid' + str(uuid.uuid4()), startTime, endTime)
+        doc.wasAssociatedWith(activity, agent)
+        doc.usage(activity, entity_uber, startTime, None,
+                  {prov.model.PROV_TYPE: 'ont:aggregate'}
                   )
-        doc.usage(get_lost, resource, startTime, None,
-                  {prov.model.PROV_TYPE: 'ont:Retrieval',
-                   'ont:Query': '?type=Animal+Lost&$select=type,latitude,longitude,OPEN_DT'
-                   }
+        doc.usage(activity, entity_health, startTime, None,
+                  {prov.model.PROV_TYPE: 'ont:aggregate'}
                   )
-
-        lost = doc.entity('dat:alice_bob#lost',
-                          {prov.model.PROV_LABEL: 'Animals Lost', prov.model.PROV_TYPE: 'ont:DataSet'})
-        doc.wasAttributedTo(lost, this_script)
-        doc.wasGeneratedBy(lost, get_lost, endTime)
-        doc.wasDerivedFrom(lost, resource, get_lost, get_lost, get_lost)
-
-        found = doc.entity('dat:alice_bob#found',
-                           {prov.model.PROV_LABEL: 'Animals Found', prov.model.PROV_TYPE: 'ont:DataSet'})
-        doc.wasAttributedTo(found, this_script)
-        doc.wasGeneratedBy(found, get_found, endTime)
-        doc.wasDerivedFrom(found, resource, get_found, get_found, get_found)
+        doc.wasAttributedTo(entity_result, agent)
+        doc.wasGeneratedBy(entity_result, activity, endTime)
+        doc.wasDerivedFrom(entity_result, entity_health, activity, activity, activity)
+        doc.wasDerivedFrom(entity_result, entity_uber, activity, activity, activity)
 
         repo.logout()
 
