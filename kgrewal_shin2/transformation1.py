7b61d7f3f6665db37bd4855562d4fd0ad75d980d
Stephanie Shin <steph.shin2@gmail.com>
2019-03-04 19:18:13 -0500

    streets_without_landmarks transformation provenance

--- kgrewal_shin2/transformation1.py
+++ kgrewal_shin2/transformation1.py
@@ -72,29 +72,46 @@ class transformation1():
         return {"start": startTime, "end": endTime}
 
 
+    @staticmethod
+    def provenance(doc=prov.model.ProvDocument(), startTime=None, endTime=None):
+        # Set up the database connection.
+        client = dml.pymongo.MongoClient()
+        repo = client.repo
+        repo.authenticate('kgrewal_shin2', 'kgrewal_shin2')
+        doc.add_namespace('alg', 'http://datamechanics.io/algorithm/')  # The scripts are in <folder>#<filename> format.
+        doc.add_namespace('dat', 'http://datamechanics.io/data/')  # The data sets are in <user>#<collection> format.
+        doc.add_namespace('ont',
+                          'http://datamechanics.io/ontology#')  # 'Extension', 'DataResource', 'DataSet', 'Retrieval', 'Query', or 'Computation'.
+        doc.add_namespace('log', 'http://datamechanics.io/log/')  # The event log.
+        doc.add_namespace('bdp', 'https://data.cityofboston.gov/resource/')
 
-    # @staticmethod
-    # def provenance(doc=prov.model.ProvDocument(), startTime=None, endTime=None):
-    #     '''
-    #         Create the provenance document describing everything happening
-    #         in this script. Each run of the script will generate a new
-    #         document describing that invocation event.
-    #         '''
-    #
-    #     # Set up the database connection.
-    #     client = dml.pymongo.MongoClient()
-    #     repo = client.repo
-    #     repo.authenticate('kgrewal_shin2', 'kgrewal_shin2')
-    #     doc.add_namespace('alg', 'http://datamechanics.io/algorithm/')  # The scripts are in <folder>#<filename> format.
-    #     doc.add_namespace('dat', 'http://datamechanics.io/data/')  # The data sets are in <user>#<collection> format.
-    #     doc.add_namespace('ont',
-    #                       'http://datamechanics.io/ontology#')  # 'Extension', 'DataResource', 'DataSet', 'Retrieval', 'Query', or 'Computation'.
-    #     doc.add_namespace('log', 'http://datamechanics.io/log/')  # The event log.
-    #     doc.add_namespace('bdp', 'https://data.cityofboston.gov/resource/')
-    #
+        this_script = doc.agent('alg:kgrewal_shin2#street_name_prov',
+                                {prov.model.PROV_TYPE: prov.model.PROV['SoftwareAgent'], 'ont:Extension': 'py'})
+        resource = doc.entity('bdp:wc8w-nujj',
+                              {'prov:label': '311, Service Requests', prov.model.PROV_TYPE: 'ont:DataResource',
+                               'ont:Extension': 'json'})
+        get_streets = doc.activity('log:uuid' + str(uuid.uuid4()), startTime, endTime)
+
+        doc.wasAssociatedWith(get_streets, this_script)
+        doc.usage(get_streets, resource, startTime, None,
+                  {prov.model.PROV_TYPE: 'ont:Retrieval',
+                   'ont:Query': '?type=Streets+Without+Landmarks&$select=full_name,gender,zipcode,street_name'
+                   }
+                  )
+
+        streets = doc.entity('dat:kgrewal_shin2#streets',
+                          {prov.model.PROV_LABEL: 'Streets Without Landmarks', prov.model.PROV_TYPE: 'ont:DataSet'})
+        doc.wasAttributedTo(streets, this_script)
+        doc.wasGeneratedBy(streets, get_streets, endTime)
+        doc.wasDerivedFrom(streets, resource, get_streets, get_streets, get_streets)
+
+        repo.logout()
+
+        return doc
+
 
 transformation1.execute()
-# doc = ProvenanceModel.provenance()
-# print(doc.get_provn())
-# print(json.dumps(json.loads(doc.serialize()), indent=4))
+doc = transformation1.provenance()
+print(doc.get_provn())
+print(json.dumps(json.loads(doc.serialize()), indent=4))
 
