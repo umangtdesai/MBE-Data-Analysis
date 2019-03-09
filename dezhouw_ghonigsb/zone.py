f19f4fc11bd3e73425458bfc24abc77066c04f4f
dezhouw <hongsinwang@gmail.com>
2019-03-08 21:35:37 -0500

    modify transformation in zone.py

--- dezhouw_ghonigsb/zone.py
+++ dezhouw_ghonigsb/zone.py
@@ -1,5 +1,5 @@
 # zone.py
-# created on Feb 27, 2019
+# created on Mar 8, 2019
 
 import urllib.request
 import json
@@ -13,13 +13,13 @@ import traceback
 import csv
 import xmltodict
 import io
+from shapely.geometry import Point
+from shapely.geometry.polygon import Polygon
 
 class zone(dml.Algorithm):
 	contributor = "dezhouw_ghonigsb"
 	reads       = []
-	writes      = [
-				   "zoning_subdistricts",
-				   "zillow_boston_neighborhood"]
+	writes      = ["zone"]
 
 	@staticmethod
 	def execute(trial = False):
@@ -30,23 +30,20 @@ class zone(dml.Algorithm):
 		repo   = client.repo
 		repo.authenticate("dezhouw_ghonigsb", "dezhouw_ghonigsb")
 
-		# Drop all collections
-		collection_names = repo.list_collection_names()
-		for collection_name in collection_names:
-			repo.dropCollection(collection_name)
-
+		# # Drop all collections
+		# collection_names = repo.list_collection_names()
+		# for collection_name in collection_names:
+		# 	repo.dropCollection(collection_name)
 
 		# zoning_subdistricts
 		collection_name = "zoning_subdistricts"
 		url             = "https://opendata.arcgis.com/datasets/b601516d0af44d1c9c7695571a7dca80_0.geojson"
 		gcontext        = ssl.SSLContext()
 		response        = urllib.request.urlopen(url, context=gcontext).read().decode("utf-8")
-		r               = json.loads(response)
-		repo.createCollection(collection_name)
-		repo["dezhouw_ghonigsb."+collection_name].insert_one(r)
+		r_zone          = json.loads(response)
 		print("Success: [{}]".format(collection_name))
 
-		# census
+		# zillow_boston_neighborhood
 		collection_name = "zillow_boston_neighborhood"
 		params          = {
 			'zws-id'   : dml.auth["census"]["Zillow API"]["zws-id"],
@@ -58,11 +55,44 @@ class zone(dml.Algorithm):
 		url             = "https://www.zillow.com/webservice/GetRegionChildren.htm?"
 		gcontext        = ssl.SSLContext()
 		response        = urllib.request.urlopen(url, args, context=gcontext).read().decode("utf-8")
-		r               = xmltodict.parse(response)
-		repo.createCollection(collection_name)
-		repo["dezhouw_ghonigsb."+collection_name].insert_one(r)
+		r_zillow        = xmltodict.parse(response)
 		print("Success: [{}]".format(collection_name))
 
+		collection_name = "zone"
+		repo.dropCollection(collection_name)
+		repo.createCollection(collection_name)
+
+		for region in r_zillow["RegionChildren:regionchildren"]["response"]["list"]["region"]:
+			name = region["name"]
+			if ("zindex" not in region.keys()):
+				continue
+			money = float(region["zindex"]["#text"])
+			x = float(region["latitude"])
+			y = float(region["longitude"])
+			point = Point(x, y)
+			found = False
+			for zone in r_zone["features"]:
+				district_name = zone["properties"]["DISTRICT"]
+				for list_coord in zone["geometry"]["coordinates"]:
+					try:
+						polygon = Polygon(list_coord)
+						if (polygon.contains(point)):
+							found = True
+							entry = {
+								"name": name,
+								"money": money,
+								"latitude": x,
+								"longitude": y,
+								"district": district_name
+							}
+							repo["dezhouw_ghonigsb."+collection_name].insert_one(entry)
+							break
+					except:
+						continue
+				if (found):
+					break
+
+		repo["dezhouw_ghonigsb."+collection_name].metadata({'complete':True})
 
 
         # Disconnect database for data safety
@@ -82,56 +112,42 @@ class zone(dml.Algorithm):
 		# extra resources
 		doc.add_namespace('opd', 'https://opendata.arcgis.com/datasets/')
 		doc.add_namespace('zil', 'https://www.zillow.com/webservice/')
-		doc.add_namespace('gov', 'http://datamechanics.io/data/ghonigsb_dezhouw/')
 
-		this_script   = doc.agent('alg:dezhouw_ghonigsb#flood', {prov.model.PROV_TYPE:prov.model.PROV['SoftwareAgent'], 'ont:Extension':'py'})
+		this_script   = doc.agent('alg:dezhouw_ghonigsb#zone', {prov.model.PROV_TYPE:prov.model.PROV['SoftwareAgent'], 'ont:Extension':'py'})
 		resource1     = doc.entity('opd:boston', {'prov:label':'Opendata Website', prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extension':'geojson'})
 		resource2     = doc.entity('zil:boston', {'prov:label':'Zillow API',       prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extension':'xml'})
-		resource3     = doc.entity('gov:boston', {'prov:label':'MassGov',          prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extension':'csv'})
-		get_seaAnnual = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
-		get_seaTide   = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
 		get_zoning    = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
 		get_zillow    = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
-		get_massGov   = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
 
-		doc.wasAssociatedWith(get_zoning,    this_script)
-		doc.wasAssociatedWith(get_zillow,    this_script)
+		doc.wasAssociatedWith(get_zoning, this_script)
+		doc.wasAssociatedWith(get_zillow, this_script)
 
-
+		doc.usage(get_zoning,    resource1, startTime, None,
+			      {prov.model.PROV_TYPE:'ont:Retrieval',
+			      'ont:Query':'?type=Boston+Neighborhood'})
 		doc.usage(get_zillow,    resource2, startTime, None,
 			      {prov.model.PROV_TYPE:'ont:Retrieval',
 			      'ont:Query':'?type=Boston+Neighborhood'})
 
 
-		zoning    = doc.entity('dat:dezhouw_ghonigsb#zoning_subdistricts',
-								{prov.model.PROV_LABEL:'Zone Subdistricts',
+		zone    = doc.entity('dat:dezhouw_ghonigsb#zone',
+								{prov.model.PROV_LABEL:'Zone Price Info',
 								 prov.model.PROV_TYPE:'ont:DataSet'})
-		doc.wasAttributedTo(zoning, this_script)
-		doc.wasGeneratedBy(zoning, get_zoning, endTime)
-		doc.wasDerivedFrom(zoning, resource1, get_zoning, get_zoning, get_zoning)
 
-		zillow    = doc.entity('dat:dezhouw_ghonigsb#zillow_boston_neighborhood',
-								{prov.model.PROV_LABEL:'Boston Neighborhood',
-								 prov.model.PROV_TYPE:'ont:DataSet'})
-		doc.wasAttributedTo(zillow, this_script)
-		doc.wasGeneratedBy(zillow, get_zillow, endTime)
-		doc.wasDerivedFrom(zillow, resource1, get_zillow, get_zillow, get_zillow)
-
-		massGov   = doc.entity('dat:dezhouw_ghonigsb#massgov_most_recent_peak_hr',
-								{prov.model.PROV_LABEL:'Peak Hours',
-								 prov.model.PROV_TYPE:'ont:DataSet'})
-		doc.wasAttributedTo(massGov, this_script)
-		doc.wasGeneratedBy(massGov, get_massGov, endTime)
-		doc.wasDerivedFrom(massGov, resource1, get_massGov, get_massGov, get_massGov)
+		doc.wasAttributedTo(zone, this_script)
+		doc.wasGeneratedBy(zone, get_zoning, endTime)
+		doc.wasDerivedFrom(zone, resource1, get_zoning, get_zoning, get_zoning)
+		doc.wasGeneratedBy(zone, get_zillow, endTime)
+		doc.wasDerivedFrom(zone, resource1, get_zillow, get_zillow, get_zillow)
 
 		return doc
 
 if __name__ == '__main__':
 	try:
-		print(flood.execute())
-		doc = flood.provenance()
-		print(doc.get_provn())
-		print(json.dumps(json.loads(doc.serialize()), indent=4))
+		print(zone.execute())
+		# doc = zone.provenance()
+		# print(doc.get_provn())
+		# print(json.dumps(json.loads(doc.serialize()), indent=4))
 	except Exception as e:
 		traceback.print_exc(file = sys.stdout)
 	finally:
